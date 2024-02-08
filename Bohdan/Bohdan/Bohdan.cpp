26610ac0cc01e65214b3a26893fd26df33e3ad3c
BohdanHoriachyi <horiachyi.bohdan@chnu.edu.ua>
2024-02-08 20:15:42 +0200

    MyFirstcommit

---
+++ Bohdan/Bohdan/Bohdan.cpp
@@ -0,0 +1,11 @@
+// Bohdan.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!I like sosage!\n";
+}
+
+
